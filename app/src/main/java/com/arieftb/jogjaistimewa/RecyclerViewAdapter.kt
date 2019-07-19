22c11ee8d2e6e3a2a989444cb1f8b4c3e85462d9
arieftb <turbagus14@gmail.com>
2019-07-19 20:43:42 +0700

    [arieftb] - adding recyclerView adapter

---
+++ app/src/main/java/com/arieftb/jogjaistimewa/RecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.arieftb.jogjaistimewa
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(private val context: Context, private val culinaryList: List<Culinary>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item_culinary, parent, false))
+
+    override fun getItemCount(): Int = culinaryList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(culinaryList[position])
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val culinaryName = view.findViewById<TextView>(R.id.text_culinary_name)
+        private val culinaryImage = view.findViewById<ImageView>(R.id.image_culinary_thumb)
+
+        fun bind(culinary: Culinary) {
+            culinaryName.text = culinary.culinaryName
+            culinaryImage.apply {
+                Picasso.get().load(culinary.culinaryImage).into(this)
+            }
+        }
+    }
+}
